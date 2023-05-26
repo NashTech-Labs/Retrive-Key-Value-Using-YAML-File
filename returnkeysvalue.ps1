ff672d9766bf0d5536c57882d171ab61eb8298de
harshit0015 <76742193+harshit0015@users.noreply.github.com>
2023-05-26 11:01:50 +0530

    Update returnkeysvalue.ps1

--- returnkeysvalue.ps1
+++ returnkeysvalue.ps1
@@ -1,4 +1,4 @@
-$pipelineID =ConvertFrom-Yaml (Get-Content -Path /home/knoldus/Naas-JSON/Pipeline_id.yml -Raw)#Get-Content -Path Pipeline_id.yml -Force | ConvertFrom-Yaml #ConvertFrom-Yaml (Get-Content -Path /home/knoldus/Naas-JSON/Pipeline_id.yml -Raw)# Invoke-RestMethod -Uri "https://raw.githubusercontent.com/muzakkirsaifi123/DCT-json/main/pipelineid.json" `
+$pipelineID =ConvertFrom-Yaml (Get-Content -Path /home/knoldus/Naas-JSON/Pipeline_id.yml -Raw)
 | Select-Object -ExpandProperty "resources" `
 | Where-Object { $_.resourceType -eq "storage-account" } `
 | Group-Object -Property "pipelineID" `
